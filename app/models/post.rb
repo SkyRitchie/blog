81af95584ef87d653df8df126d7bc47a8f74761d
schuyler Ritchie <schuylerelizabethritchie@gmail.com>
2015-06-24 10:10:46 -0400

    validate comments and posts

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,5 +1,8 @@
 class Post < ActiveRecord::Base
 	has_many :comments
+
+	validates :body, presence: true
+	validates :title, presence: true
 	def published?
 		publication.present? and url.present? and published_at.present?
 	end
